4f619d3d13aee424e13511ee35d69a4bbaced3e3
dhj <cloudfyy@hotmail.com>
2023-02-28 07:29:10 +0800

    webhook ver to 0.31

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-docker build -t cloudfyy/akswebhook:0.30 .
-docker push cloudfyy/akswebhook:0.30
+docker build -t cloudfyy/akswebhook:0.31 .
+docker push cloudfyy/akswebhook:0.31
 
-cd tlsgenerator
-docker build -t cloudfyy/akswebhookcert:v02 .
-docker push cloudfyy/akswebhookcert:v02
-cd ..
+#cd tlsgenerator
+#docker build -t cloudfyy/akswebhookcert:v02 .
+#docker push cloudfyy/akswebhookcert:v02
+#cd ..
