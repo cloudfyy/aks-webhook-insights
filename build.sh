520f5dce0b933a54abd67400b5778848ef379ed5
dhj <cloudfyy@hotmail.com>
2023-02-28 09:07:39 +0800

    fix klog info bug

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-docker build -t cloudfyy/akswebhook:0.32 .
-docker push cloudfyy/akswebhook:0.32
+docker build -t cloudfyy/akswebhook:0.33 .
+docker push cloudfyy/akswebhook:0.33
 
 #cd tlsgenerator
 #docker build -t cloudfyy/akswebhookcert:v02 .
