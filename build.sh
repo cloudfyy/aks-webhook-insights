ecd4a1356303cb6888159b6e35d8eb118bed1741
dhj <cloudfyy@hotmail.com>
2023-03-01 15:08:07 +0800

    upgrade image to 0.36

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-docker build -t cloudfyy/akswebhook:0.35 .
-docker push cloudfyy/akswebhook:0.35
+docker build -t cloudfyy/akswebhook:0.36 .
+docker push cloudfyy/akswebhook:0.36
 
 #cd tlsgenerator
 #docker build -t cloudfyy/akswebhookcert:v02 .
