e71972b92f42b1100a2ede2403f62fc719d77604
dhj <cloudfyy@hotmail.com>
2023-03-03 16:47:25 +0800

    helm package script

--- helm/build.sh
+++ helm/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-
+APPVER=1.0.0
+Version=1.0.0
 # run helm package
 
 mkdir ../helm_package
 helm lint 
-helm package . -d ../helm_package --app-version 0.1.2 --version 0.1.2 
+helm package . -d ../helm_package --app-version ${APPVER} --version ${Version}
