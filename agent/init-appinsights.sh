eda030d8a6d839a1201901d8b951e14e7f1ca08f
dhj <cloudfyy@hotmail.com>
2023-03-05 08:11:59 +0800

    add agent image docker definition and update build scrips

---
+++ agent/init-appinsights.sh
@@ -0,0 +1,6 @@
+#!/bin/sh 
+# This script is used to initialize Application Insights for Java agent
+# It will be executed by the agent when the agent is started
+wget https://github.com/microsoft/ApplicationInsights-Java/releases/download/3.4.10/applicationinsights-agent-3.4.10.jar
+sed -i "s|ROLE_NAME|${ROLE_NAME}|g" applicationinsights.json
+sed -i "s|CONNECTION_STRING|${CONNECTION_STRING}|g" applicationinsights.json
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ TAG="1.00"
 docker build -t cloudfyy/akswebhook:${TAG} .
 docker push cloudfyy/akswebhook:${TAG}
 
-#cd tlsgenerator
-#docker build -t cloudfyy/akswebhookcert:v02 .
-#docker push cloudfyy/akswebhookcert:v02
-#cd ..
+cd ../agent
+docker build -t cloudfyy/application-insights-java-agent:${TAG} .
+docker push cloudfyy/application-insights-java-agent:${TAG}
+cd ..
