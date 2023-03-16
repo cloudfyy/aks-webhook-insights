8ae36bdbfc44403ebcf6b4d52954baa0f225d8bb
cloudfyy <cloudfyy@hotmail.com>
2023-03-16 09:12:15 +0800

    upgrade img to 1.0.6

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-TAG="1.0.5"
+TAG="1.0.6"
 
 cd ./agent
 docker build -t cloudfyy/application-insights-java-agent:${TAG} .
--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@
 
 set -e
 javaagentversion=3.4.10
-imgver=1.0.5
+imgver=1.0.6
 title="app-monitoring-webhook"
 namespace="kube-system"
 environment="test"
