2a5fc9858c516c548ac05aa042f388f2526d6e68
cloudfyy <cloudfyy@hotmail.com>
2023-03-17 10:59:30 +0800

    debug ap insights json config

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-TAG="1.0.10"
+TAG="1.0.11"
 
 cd ./agent
 docker build -t cloudfyy/application-insights-java-agent:${TAG} .
--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@
 
 set -e
 javaagentversion=3.4.10
-imgver=1.0.10
+imgver=1.0.11
 title="app-monitoring-webhook"
 namespace="kube-system"
 environment="test"
