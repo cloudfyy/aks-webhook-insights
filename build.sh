f6f934cd3390e97c90ccd30998f62c814bf4cabd
cloudfyy <cloudfyy@hotmail.com>
2023-03-17 13:21:29 +0800

    upgrade agent version to 1.0.13

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-TAG="1.0.12"
+TAG="1.0.13"
 
 cd ./agent
 docker build -t cloudfyy/application-insights-java-agent:${TAG} .
--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@
 
 set -e
 javaagentversion=3.4.10
-imgver=1.0.12
+imgver=1.0.13
 title="app-monitoring-webhook"
 namespace="kube-system"
 environment="test"
