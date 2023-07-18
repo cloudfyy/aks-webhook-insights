2d614a7ee6fa0fbde9b6101d3b3c7de33389a11c
cloudfyy <cloudfyy@hotmail.com>
2023-07-19 05:45:25 +0800

    debug

--- agent/init-appinsights.sh
+++ agent/init-appinsights.sh
@@ -2,6 +2,6 @@
 # This script is used to initialize Application Insights for Java agent
 # It will be executed by the agent when the agent is started
 # wget https://github.com/microsoft/ApplicationInsights-Java/releases/download/3.4.10/applicationinsights-agent-3.4.10.jar
-echo ${CONNECTION_STRING}, ${ROLE_NAME} >debug.log
+echo ${CONNECTION_STRING}, ${ROLE_NAME} > /config/debug.log
 sed -i "s|ROLE_NAME|${ROLE_NAME}|g" applicationinsights.json
 sed -i "s|CONNECTION_STRING|${CONNECTION_STRING}|g" applicationinsights.json
--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-imgver=1.0.27
+imgver=1.0.28
 title="app-monitoring-webhook"
 namespace="kube-system"
 environment="test"
